e52b7fa7d8dadd70277db59a0602839bf9173301
Azure <tp3more21@gmail.com>
2024-03-03 11:29:46 +0200

    PULL

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import General from './components/paymentFirstscreen/buttomComponent/general';
+import Address from './components/paymentFirstscreen/buttomComponent/address';
+import ContactDetails from './components/paymentFirstscreen/buttomComponent/contactDetails';
+import ShippingAddress from './components/paymentFirstscreen/buttomComponent/shippingAddress';
+import CompanyDetails from './components/paymentFirstscreen/buttomComponent/companyDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <CompanyDetails/> */}
+      <General/>
     </div>
   );
 }
---
+++ src/components/paymentFirstscreen/buttomComponent/shippingAddress.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ShippingAddress() {
+  return (
+    <div>
+        {/* ShippingAddress */}
+        
+        </div>
+  )
+}
