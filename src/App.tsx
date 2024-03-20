2cbfc9ae61cd6bdf5fc178eea5bde8793362a372
sariHeber <sa0548503880@gmail.com>
2024-03-20 11:06:32 +0200

    WIP fix style & conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { TopHeader } from './components/TopHeader';
 import { BaseScreen } from "./components/base-screen";
 import { BrowserRouter } from "react-router-dom";
-import { Login } from "./components/login";
 
 function App() {
   return (
     <div>
-        <div>
-      <TopHeader></TopHeader>
       <BrowserRouter>
         <BaseScreen />
-        <Login/>
       </BrowserRouter>
-    </div>
     </div>
   );
 }
