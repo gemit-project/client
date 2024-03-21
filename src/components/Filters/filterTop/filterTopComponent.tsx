8989a4608961d14583c25ef9abd2527048636250
tzivya <tl4340@gmail.com>
2024-03-21 13:16:10 +0200

    in order to do pull

--- src/components/Filters/filterTop/filterTopComponent.tsx
+++ src/components/Filters/filterTop/filterTopComponent.tsx
@@ -5,23 +5,67 @@ import FilterBasicComponent, { FilterAdvancedComponent, FilterGeneralComponent }
 export default function FilterTopComponent() {
     return (
         <>
+            <div className='reka'>
+
+                <br />
+                <button style={{
+                    backgroundColor: '#D200FF'
+                }} onClick={() => { <FilterBasicComponent /> }}>Basic</button>
+
+                <button style={{
+                    backgroundColor: '#FF00FF'
+                }} >Advanced
+                      {/* <FilterAdvancedComponent /> */}
+                 </button>
+
+                <button style={{
+                    backgroundColor: '#00B2FF'
+                }} >   General
+                {/* <FilterGeneralComponent /> */}
+                 </button>
+
+                <button className='rightButton'><CiFilter /> Apply </button>
+                <button className='rightButton'> x </button>
+                <br /><br />
+            </div>
             <br />
-            <button style={{
-                backgroundColor: 'D200FF'
-            }} ><FilterBasicComponent /></button>
+            <div className='block1Input'>
+            <div className='block1'>
+                Shape<br/><br/>
+                <div className="inside" >first</div>
+                <div className="inside" >first</div>
+                <div className="inside" >first</div>
+                <div className="inside" >first</div>
 
-            <button style={{
-                backgroundColor: 'rgb(252, 127, 200)'
-            }} >  <FilterAdvancedComponent /> </button>
+            </div>
+            <div>
+                <input></input>
+                <input></input>
+            </div>
+            </div><br />
+            <div className='block12'>
+            <div className='block2'>
+                Clarity<br/><br/>
+                <div className="inside">second</div>
+                <div className="inside">second</div>
+                <div className="inside">second</div>
+                <div className="inside">second</div>
 
-            <button style={{
-                backgroundColor: 'rgb(113, 227, 247)'
-            }} >   <FilterGeneralComponent /> </button>
+            </div><br />
+   
+            <div className='block3'>
+                    Color<br/><br/>
+                    <div className="inside">third</div>
+                    <div className="inside">third</div>
+                    <div className="inside">third</div>
+                    <div className="inside">third</div>
 
-            <button className='rightButton'><CiFilter /> Apply </button>
-            <button className='rightButton'> x </button>
+                </div><br />
+                </div>
 
         </>
+
     )
 }
-
+//כשלוחצים על כפתור שיציג את הקומפוננטה שלו וכשלוחצים 
+//על כפתור אחר שיסגור את הקומפוננטה הקודמת ויפתח את זאת
