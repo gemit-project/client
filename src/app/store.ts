f4b43621d0355fdf0f8aa88718515c38567ca14f
Pninaschwartz <pnina7560@gmail.com>
2024-04-03 10:30:45 +0300

    more files

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
 import { UserSlice } from "./slices/UserSlice";
 
-export const store = configureStore({
+export const reduxStore = configureStore({
     reducer: {
         user: UserSlice.reducer,
 
