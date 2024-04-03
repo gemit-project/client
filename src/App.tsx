5504659c9235ecaa2652db671c54975bea16686e
sariHeber <sa0548503880@gmail.com>
2024-04-03 12:18:05 +0300

    add tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { BaseScreen } from "./components/base-screen";
 import { BrowserRouter } from "react-router-dom";
+import "./output.css";
 
 function App() {
   return (
