5fd75c58307add02b2055d037ac2ef34fff32a9c
Pninaschwartz <pnina7560@gmail.com>
2024-04-04 10:59:33 +0300

    remove not needed imports

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -1,19 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { ThemeProvider, createTheme } from '@mui/material';
-import { LogInUser } from "../Types/LoginUser";
 import 'sharetribe-flex-sdk';
-import { VisibilityOff, Visibility, ArrowForwardIos } from '@mui/icons-material';
-import { InputAdornment, IconButton, Dialog, DialogContent, DialogActions, DialogContentText, DialogTitle } from '@mui/material';
+import {  Dialog, DialogContent, DialogActions, DialogContentText, DialogTitle } from '@mui/material';
 import { sdk } from '../config/sharetribeSDK.config';
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom'
-import { Routing } from '../utils/routing';
 
 export const PasswordReset = (props: any) => {
 
