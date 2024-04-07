670c1d7fd62b0ff5e40b32a506eb2d862a5ffc09
nechamy <n0548566066@gmail.com>
2024-04-07 17:37:10 +0300

    routing for button

--- src/utils/routing.tsx
+++ src/utils/routing.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard } from "../components/Dashboard";
-import { Search } from "../components/Search";
-import { CheckOut } from "../components/CheckOut";
-import { LogOut } from "../components/LogOut";
-import { Setting } from "../components/Setting";
-import { Chat } from "../components/Chat";
+import { Dashboard } from "../components/SideBar/Dashboard";
+import { Search } from "../components/SideBar/Search";
+import { CheckOut } from "../components/SideBar/CheckOut";
+import { LogOut } from "../components/SideBar/LogOut";
+import { Setting } from "../components/SideBar/Setting";
+import { Chat } from "../components/SideBar/Chat";
+import { Compare } from "../components/SideBar/Search/Button-Section/Compare";
+import { Reset } from "../components/SideBar/Search/Button-Section/Reset";
+import { Filter } from "../components/SideBar/Search/Button-Section/Filter";
+import { View } from "../components/SideBar/Search/Button-Section/View";
+import { Sort } from "../components/SideBar/Search/Button-Section/Sort";
 
 export const Routing: React.FC = () => {
-    return (
-      <>
-                  <Routes>
-            <Route path="/Dashboard" element={< Dashboard />} />
-            <Route path="/Search" element={< Search/>} />
-            <Route path="/CheckOut" element={< CheckOut />} />
-            <Route path="/LogOut" element={< LogOut />} />
-            <Route path="/Setting" element={< Setting />}/>
-            <Route path="/Chat" element={< Chat />}/>
-          </Routes>
-              </>
-    );
-  };
+  return (
+    <>
+      <Routes>
+        <Route path="/Dashboard" element={< Dashboard />} />
+        <Route path="/Search" element={< Search />}>
+           <Route path="/Search/Compare" element={< Compare />} />
+           <Route path="/Search/Filter" element={< Filter />} />
+           <Route path="/Search/Reset" element={< Reset />} />
+           <Route path="/Search/View" element={< View />} />
+           <Route path="/Search/Sort" element={< Sort />} />
+        </Route> 
+        <Route path="/CheckOut" element={< CheckOut />} />
+        <Route path="/LogOut" element={< LogOut />} />
+        <Route path="/Setting" element={< Setting />} />
+        <Route path="/Chat" element={< Chat />} />
+      </Routes>
+    </>
+  );
+};
