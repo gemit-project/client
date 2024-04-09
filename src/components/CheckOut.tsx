7d87ec5453678d80741d65b54f4d1ca366a88cdf
yafa <yafa0694@gmail.com>
2024-04-09 13:29:25 +0300

    commi to the icons

--- src/components/CheckOut.tsx
+++ src/components/CheckOut.tsx
@@ -1,15 +1,25 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 import { Link, Outlet, useNavigate } from "react-router-dom";
+import boldCircle from './icons/boldCircle.svg'
+import simpleCircle from './icons/simpleCircle.svg'
 
 export const CheckOut: React.FC = () => {
-    const checkoutPages = ['General', 'ShippingAndInsurance', 'FundingOptions', 'Payment', 'Confirmation']
+    const checkoutpPages = ['General', 'ShippingAndInsurance', 'FundingOptions', 'Payment', 'Confirmation']
+    const checkoutPages = [
+
+        { text: 'General', nav: 'General' },
+        { text: 'Shipping&Insurance', nav: 'ShippingAndInsurance' },
+        { text: 'FundingOptions', nav: 'FundingOptions' },
+        { text: 'Payment', nav: 'Payment' },
+        { text: 'Confirmation', nav: 'Confirmation' }
+    ]
     const location = useLocation();
     const navigate = useNavigate();
 
-    useEffect(()=>{
+    useEffect(() => {
         navigate('General')
-    },[])
+    }, [])
 
     return (
 
@@ -18,8 +28,8 @@ export const CheckOut: React.FC = () => {
                 <div style={{ marginLeft: '0.8vw', zIndex: -1, width: '80vw', position: 'absolute', border: '0.5vh #0000FF solid' }}></div>
                 {
                     checkoutPages.map((page, i) => (
-                        <Link to={checkoutPages[i]}>
-                            <Circle current={location.pathname.endsWith(`/${checkoutPages[i]}`) ? true : false} text={checkoutPages[i]} def={location.pathname.endsWith(`/CheckOut`) && checkoutPages[i] === 'General' ? true : false} />
+                        <Link to={page.nav}>
+                            <Circle current={location.pathname.endsWith(`/${page.nav}`) ? true : false} text={page.text} def={location.pathname.endsWith(`/CheckOut`) && page.text === 'General' ? true : false} />
                         </Link>
 
                     ))
@@ -32,18 +42,18 @@ export const CheckOut: React.FC = () => {
 
 const Circle: React.FC<{ current: boolean, text: string, def: boolean }> = ({ current, text, def }) => {
 
-    return (<div style={{ marginLeft: '-1vw', marginBottom: '-10vh', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex', position: 'relative', alignContent: 'center' }}>
-        {current || def ? <div style={{ width: '3vw', height: '3vw', position: 'relative', justifyContent: 'center', alignItems: 'center', alignContent: 'start', display: 'inline-flex' }}>
-            <div style={{ width: '2.8vw', height: '2.8vw', position: 'absolute', background: '#B4B4FF', borderRadius: 9999 }} />
-            <div style={{ alignItems: 'center', width: '1.7vw', height: '1.7vw', position: 'absolute', background: '#0000FF', borderRadius: 9999, border: '0px #E6E6E6 solid' }} />
-        </div> :
-            <div style={{ width: '1.7vw', height: '1.7vw', background: '#C0C0FF', borderRadius: 9999, position: 'relative' }} />}
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-            {current || def ? <div style={{ textAlign: 'center', color: 'black', fontSize: '17px', fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word' }}>{text}</div> :
-                <div style={{ textAlign: 'center', color: 'black', fontSize: '15px', fontFamily: 'Poppins', fontWeight: '450', wordWrap: 'break-word', position: 'relative' }}>{text}</div>
-            }
+    return (
+        <div style={{ marginLeft: '-1vw', marginBottom: '-10vh', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex', position: 'relative', alignContent: 'center' }}>
+            {current || def ? <div style={{ width: '3vw', height: '3vw', position: 'relative', justifyContent: 'center', alignItems: 'center', alignContent: 'start', display: 'inline-flex' }}>
+                <img src={boldCircle.toString()}></img>
+            </div> :
+                <img src={simpleCircle.toString()}></img>}
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                {current || def ? <div style={{ textAlign: 'center', color: 'black', fontSize: '17px', fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word' }}>{text}</div> :
+                    <div style={{ marginTop: '0.7vh', textAlign: 'center', color: 'black', fontSize: '15px', fontFamily: 'Poppins', fontWeight: '450', wordWrap: 'break-word', position: 'relative' }}>{text}</div>
+                }
+            </div>
         </div>
-    </div>
     )
 }
 
