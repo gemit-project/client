e00c1720d084603d6847224c61155becf6ee5b36
Pninaschwartz <pnina7560@gmail.com>
2024-04-30 23:53:04 +0300

    Featers Chart

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -3,28 +3,33 @@ import "./ProductPage.css";
 import { sdk } from "../config/sharetribeSDK.config";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { Box, Button, IconButton, TextField, Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import back from "../assets/icons/product-icons/back.svg";
 import lock from "../assets/icons/product-icons/Vector.svg";
 import madalya from "../assets/icons/product-icons/Madalya.svg";
 import arrowDount from "../assets/icons/product-icons/Group 1652.svg";
-import love from "../assets/icons/personal-area-icons/heart.svg";
-const { UUID } = require("sharetribe-flex-sdk").types;
+import fullLove from "../assets/icons/personal-area-icons/heart.svg";
+import emptyLove from "../assets/icons/product-icons/EmptyLove.svg";
+import { DiamodsFeaters } from "./Diamond’sFeatures";
 
+const { UUID } = require("sharetribe-flex-sdk").types;
+const propsCol1 = ["Shape", "SIZE", "Color", "Clarity"];
+const propsCol2 = ["CUT", "polish", "Symmetry", "Fluorescence"];
 export const ProductPage = () => {
   const navigation = useNavigate();
   const params = useParams();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const currentUser = useSelector((state: any) => state.user.currentUser);
-  const imagges = useSelector((state: any) => state.user.images);
+  // const imagges = useSelector((state: any) => state.user.images);
   const [imgsUuid, setImgsUuid] = useState<
     Array<{ id: typeof UUID; type: "" }>
   >([]);
   const [imgsUrl, setImgsUrl] = useState<Array<string>>([]);
   const [image, setImgList] = useState<Array<any>>([]);
-  const [file, setFile] = useState<File | undefined | null>();
+  // const [file, setFile] = useState<File | undefined | null>();
   const [selected, setSelected] = useState<string>("");
+  const [isClick, setClick] = useState<boolean>(false);
   useEffect(() => {
     fff();
   }, []);
@@ -63,7 +68,7 @@ export const ProductPage = () => {
               navigation("/Search");
             }}
           >
-            <img  src={back}></img>
+            <img src={back}></img>
           </IconButton>
         </div>
 
@@ -82,32 +87,19 @@ export const ProductPage = () => {
                 ></img>
               ))}
             </div>
-           <div >
-            <img className="current" src={selected}></img>
-            <div className="addwish">
-              <IconButton >
-               <img src={love}></img>
-              </IconButton>
-          <Typography>Add To WishList</Typography>
-        </div>
-        <div className="frame2012">
-          <div className="frame2010">
-            <div className="fetures">
-                 <div className="frame2398">
-                  <div className="frame2397">
-                    <Typography className="featers">Diamond’s Features</Typography>
-                    <Typography>ee</Typography>
-                  </div>
-                  <Button className="buto">Fill AAAAAAAAAAAAAAAAAA</Button>
-                 </div>
+            <div>
+              <img
+                className="current"
+                src={selected ? selected : imgsUrl[0]}
+              ></img>
+              <div className="addwish">
+                <IconButton onClick={() => setClick((isClick) => !isClick)}>
+                  <img src={isClick ? fullLove : emptyLove}></img>
+                </IconButton>
+                <Typography>Add To WishList</Typography>
+              </div>
+              <DiamodsFeaters />
             </div>
-          </div>
-        </div>
-        <div className="diamondFeters">
-
-        </div>
-           </div> 
-         
           </div>
           <div className="frame2005">
             <label
@@ -135,27 +127,26 @@ export const ProductPage = () => {
             </div>
             <div className="frame20055">
               <div className="frame1970">
-                  <div className="frame2363">
-                    <div className="frame1967">
-                      <Typography className="tipp">S/CR</Typography>
-                      <Typography className="tippp">$3333</Typography>
-                    </div>
-                    <div className="frame1967">
-                      <Typography className="tipp">Discount</Typography>
-                      <Typography className="tippp">32%</Typography>
-                    </div>
-                    <div className="frame1967">
-                      <Typography className="tipp">Total</Typography>
-                      <Typography className="tippp">$4321</Typography>
-                    </div>
-                  
+                <div className="frame2363">
+                  <div className="frame1967">
+                    <Typography className="tipp">S/CR</Typography>
+                    <Typography className="tippp">$3333</Typography>
                   </div>
+                  <div className="frame1967">
+                    <Typography className="tipp">Discount</Typography>
+                    <Typography className="tippp">32%</Typography>
+                  </div>
+                  <div className="frame1967">
+                    <Typography className="tipp">Total</Typography>
+                    <Typography className="tippp">$4321</Typography>
+                  </div>
+                </div>
               </div>
-            <div className="frame1971">
-                 <Typography className="tiip">NETORAL</Typography>
-                 <Typography className="tiipLab">LAB</Typography>
+              <div className="frame1971">
+                <Typography className="tiip">NETORAL</Typography>
+                <Typography className="tiipLab">LAB</Typography>
+              </div>
             </div>
-           </div>
             <div className="frame1988">
               <div className="frame1986">
                 <div className="frame2396">
@@ -205,10 +196,7 @@ export const ProductPage = () => {
             </div>
           </div>
         </div>
-           {/* <div className="wishing">
-          <img src={love}></img>
-          <Typography>Wishing</Typography>
-        </div> */}
+
         <Outlet></Outlet>
       </div>
     </>
