1bde862cd0072e577a47b034a3e960fa80206041
nechamy <n0548566066@gmail.com>
2024-05-01 17:47:00 +0300

    arange files and show product good

--- src/components/SideBar/Search/Search/Button-Section/Filter.tsx
+++
@@ -1,4 +0,0 @@
-export const Filter: React.FC = () => {
-    return(<>
-    </>) ;
-};
--- src/components/pages/search/Search.tsx
+++ src/components/pages/search/Search.tsx
@@ -1,123 +1,12 @@
 import './Search.css';
-import { Button_Section } from './Search/Button-Section';
+import { Button_Section } from './button-section/Button-Section';
 import love from '../../../assets/Like.svg';
 import RedLove from '../../../assets/LikeRed.png';
 import box from '../../../assets/box.svg';
-import diamond2 from '../../../assets/diamonds/2.svg';
-import diamond3 from '../../../assets/diamonds/3.svg';
-import diamond4 from '../../../assets/diamonds/4.svg';
-import diamond5 from '../../../assets/diamonds/5.svg';
-import diamond6 from '../../../assets/diamonds/6.svg';
-import diamond7 from '../../../assets/diamonds/7.svg';
 import { sdk } from '../../../config/sharetribeSDK.config';
 import { useEffect, useState } from 'react';
 
 const { UUID, LatLng, Money } = require("sharetribe-flex-sdk").types;
-
-
-const Diamonds = [
-    {
-        dianomdImg: diamond6,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond2,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond3,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 0,
-    },
-    {
-        dianomdImg: diamond4,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 2,
-    },
-    {
-        dianomdImg: diamond5,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 2,
-    },
-    {
-        dianomdImg: diamond6,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond7,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond2,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond6,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond2,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond3,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond4,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 0,
-    },
-    {
-        dianomdImg: diamond5,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 2,
-    },
-    {
-        dianomdImg: diamond6,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond7,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond2,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond5,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond6,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond7,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-    {
-        dianomdImg: diamond2,
-        diamondName: "TOTAL PRICE $3,455",
-        sideButton: 1,
-    },
-
-];
 
 const SideButton = [
     {
@@ -151,18 +40,9 @@ export const Search: React.FC = () => {
         getData()
     }, [])
 
-    useEffect(() => {
-        console.log('Compares', myCompares);
-        console.log('data', data)
-    }, [myCompares, data])
-
-    useEffect(() => {
-        console.log('LovlyDiamonds', lovlyDiamonds);
-    }, [lovlyDiamonds])
-
     const getData = () => {
         sdk.listings.query({
-             include: ["images"],
+            include: ["images"],
         }).then((res: any) => {
             setData(res.data.data.map((x: any) => {
                 return {
@@ -175,27 +55,24 @@ export const Search: React.FC = () => {
         });
     }
 
-    const myCompareDiamonds = (id: string, key: number) =>
-    {
+    const myCompareDiamonds = (id: string, key: number) => {
         for (let i = 0; i < data.length; i++) {
-            if(data[i].id.uuid == id && data[i].attributes.compare == false)
-            {
-               //data[i].attributes.compare  == true;
-               setMyCompares(myCompares.concat(id));
+            if (data[i].id.uuid == id && data[i].attributes.compare == false) {
+                data[i].attributes.compare = true;
+                setMyCompares(myCompares.concat(id));
             }
-            else
-            {
-               // data[i].attributes.compare  == false;
-               // myCompares.find(x=>x==id ? setMyCompares(myCompares.splice(id.)) :"")
+            else {
+                data[i].attributes.compare = false;
+                // myCompares.find(x=>x==id ? setMyCompares(myCompares.splice(id.)) :"")
             }
-    }}
+        }
+    }
 
-    const myLovlyDiamonds = (id: string, key: number) => 
-    {
-        data.find ( x => x.id.uuid == id ? x.attributes.lovly = !x.attributes.lovly  : "" );
-        data.find ( x => x.id.uuid == id && x.attributes.lovly ==true ? setLovlyDiamonds (lovlyDiamonds.concat(id)) :
-        //  lovlyDiamonds.find(item => item == id ? setLovlyDiamonds(lovlyDiamonds.splice(item)))
-         "");
+    const myLovlyDiamonds = (id: string, key: number) => {
+        data.find(x => x.id.uuid == id ? x.attributes.lovly = !x.attributes.lovly : "");
+        data.find(x => x.id.uuid == id && x.attributes.lovly == true ? setLovlyDiamonds(lovlyDiamonds.concat(id)) :
+            //  lovlyDiamonds.find(item => item == id ? setLovlyDiamonds(lovlyDiamonds.splice(item)))
+            "");
     }
 
     return (<>
@@ -207,7 +84,7 @@ export const Search: React.FC = () => {
                         <div className='allDiv' key={i}>
                             <div className="greyDiv">
                                 <div className='among'>
-                                    <img key={i} className={`${listing?.attributes?.compare ? 'boxImg' : ''}`} 
+                                    <img key={i} className={`${listing?.attributes?.compare ? 'boxImg' : ''}`}
                                         src={box} onClick={() => myCompareDiamonds(listing?.id?.uuid, i)} />
                                     {/* <div className='blueButton' style={{backgroundColor:SideButton[diamond.sideButton]?.color, border:SideButton[diamond.sideButton]?.border }}> */}
                                     {/* <div className='blueButtomText' 
