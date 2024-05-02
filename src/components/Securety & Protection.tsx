14428f6e4ec4d5a89cd7ad5873d8ecac5b213cbe
Pninaschwartz <pnina7560@gmail.com>
2024-05-02 23:37:11 +0300

    fixes lines& change names in css

--- src/components/Securety & Protection.tsx
+++ src/components/Securety & Protection.tsx
@@ -11,31 +11,33 @@ import secutery from "../assets/icons/product-icons/secutery.svg";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import lock from "../assets/icons/product-icons/Vector.svg";
 
+const shippingOptions =["Secure Courier Services","Private Jet Cargo Services","Air Freight"]
+const inssurance =["All-Risk","Valuable Goods","Declared Value","Parcel","Transit"];
 export const SecuretyProtection = () => {
   return (
     <>
       <div className="secutery-Protection">
-        <div  id="secutery">
-        <div className="secutery">
-          <Typography className="secutery-title">
-            <b>secutery & Protection</b>
-          </Typography>
-          <img src={lock}></img>
-        </div>
-        <p style={{ width: "540px" }}>
-          Ullamcorper eget nulla facilisi etiam dignissim diam quis. Accumsan
-          sit amet nulla facilisi morbi. Dignissim convallis aenean et tortor at
-          risus viverra adipiscing at. Pellentesque id nibh tortor id aliquet
-          lectus proin nibh nisl. Neque viverra justo nec ultrices dui sapien.
-          Gravida dictum fusce ut placerat orci nulla. Eget aliquet nibh
-          praesent tristique magna sit. Enim nec dui nunc mattis enim ut. Netus
-          et malesuada fames ac turpis egestas maecenas. Morbi blandit cursus
-          risus at ultrices mi. Morbi quis commodo odio aenean sed adipiscing
-          diam donec. Congue quisque egestas diam in arcu cursus euismod. Mi
-          proin sed libero enim sed. Orci eu lobortis elementum nibh tellus.
-          Elit duis tristique sollicitudin nibh. Vitae proin sagittis nisl
-          rhoncus mattis rhoncus urna neque.
-        </p>
+        <div id="secutery">
+          <div className="secutery">
+            <Typography className="secutery-title">
+              <b>secutery & Protection</b>
+            </Typography>
+            <img src={lock}></img>
+          </div>
+          <p style={{ width: "540px" }}>
+            Ullamcorper eget nulla facilisi etiam dignissim diam quis. Accumsan
+            sit amet nulla facilisi morbi. Dignissim convallis aenean et tortor
+            at risus viverra adipiscing at. Pellentesque id nibh tortor id
+            aliquet lectus proin nibh nisl. Neque viverra justo nec ultrices dui
+            sapien. Gravida dictum fusce ut placerat orci nulla. Eget aliquet
+            nibh praesent tristique magna sit. Enim nec dui nunc mattis enim ut.
+            Netus et malesuada fames ac turpis egestas maecenas. Morbi blandit
+            cursus risus at ultrices mi. Morbi quis commodo odio aenean sed
+            adipiscing diam donec. Congue quisque egestas diam in arcu cursus
+            euismod. Mi proin sed libero enim sed. Orci eu lobortis elementum
+            nibh tellus. Elit duis tristique sollicitudin nibh. Vitae proin
+            sagittis nisl rhoncus mattis rhoncus urna neque.
+          </p>
         </div>
         <div>
           <div className="secutery">
@@ -45,36 +47,18 @@ export const SecuretyProtection = () => {
             <img src={shipping}></img>
           </div>
           <div style={{ width: "540px" }}>
-            <Accordion>
+            {shippingOptions.map((shipping)=>(
+              <Accordion>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                Secure Courier Services
+                {shipping}
               </AccordionSummary>
               <AccordionDetails>bla bla bla ????????????</AccordionDetails>
             </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-              >
-                Private Jet Cargo Services
-              </AccordionSummary>
-              <AccordionDetails>bla bla bla ????????????</AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-              >
-                Air Freight
-              </AccordionSummary>
-              <AccordionDetails>bla bla bla ????????????</AccordionDetails>
-            </Accordion>
+           ) )}
           </div>
         </div>
         <div>
@@ -85,56 +69,18 @@ export const SecuretyProtection = () => {
             <img src={secutery}></img>
           </div>
           <div style={{ width: "540px" }}>
-            <Accordion>
+            {inssurance.map((inss)=>(
+              <Accordion>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                All-Risk Insurance
+                {inss} Insurance
               </AccordionSummary>
               <AccordionDetails>bla bla bla ????????????</AccordionDetails>
             </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-              >
-                Valuable Goods Insurance
-              </AccordionSummary>
-              <AccordionDetails>bla bla bla ????????????</AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-              >
-                Declared Value Insurance
-              </AccordionSummary>
-              <AccordionDetails>bla bla bla ????????????</AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-              >
-                Parcel Insurance
-              </AccordionSummary>
-              <AccordionDetails>bla bla bla ????????????</AccordionDetails>
-            </Accordion>{" "}
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-              >
-                Transit Insurance
-              </AccordionSummary>
-              <AccordionDetails>bla bla bla ????????????</AccordionDetails>
-            </Accordion>
+           ) )}
           </div>
         </div>
       </div>
